3a60210afa7615dd6073691fa7a928742c92de30
Nikola Jovic <jovnikola99@gmail.com>
2023-09-06 12:41:25 +0200

    06.09.2023. Kraj sekcije 12

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 import { Container, Segment, Header, Image, Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import LoginForm from "../users/LoginForm";
+import RegisterForm from "../users/RegisterForm";
 
-export default function HomePage() {
+export default observer(function HomePage() {
+  const { userStore, modalStore } = useStore();
+
   return (
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container text>
@@ -12,12 +18,38 @@ export default function HomePage() {
             alt="logo"
             style={{ marginBottom: 12 }}
           />
+          Reactivities
         </Header>
-        <Header as="h2" inverted content="Welcome to Reactivities" />
-        <Button as={Link} to="/activities" size="huge" inverted>
-          Take me to the Activities!
-        </Button>
+        {userStore.isLoggedIn ? (
+          <>
+            <Header
+              as="h2"
+              inverted
+              content={`Welcome back ${userStore.user?.displayName}`}
+            />
+            <Button as={Link} to="/activities" size="huge" inverted>
+              Go to Activities
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button
+              onClick={() => modalStore.openModal(<LoginForm />)}
+              size="huge"
+              inverted
+            >
+              Login
+            </Button>
+            <Button
+              onClick={() => modalStore.openModal(<RegisterForm />)}
+              size="huge"
+              inverted
+            >
+              Register
+            </Button>
+          </>
+        )}
       </Container>
     </Segment>
   );
-}
+});
